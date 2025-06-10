df9944882e984374f58c1d9cb40c73b7e4a5420f
DevDewangan16 <dewangandev977@gmail.com>
2025-06-10 15:17:37 +0530

    This is the basic prototype for the push notification manually using the fcm.

---
+++ app/src/main/java/com/example/fcm/PushNotificationService.kt
@@ -0,0 +1,17 @@
+package com.example.fcm
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class PushNotificationService:FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        // update server.
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        // here we can respond to received message.
+    }
+}
