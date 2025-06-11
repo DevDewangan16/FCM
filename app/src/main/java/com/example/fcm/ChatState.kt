1fbbdc1432ba92d97f23fb1ce12c195865d41b1a
DevDewangan16 <dewangandev977@gmail.com>
2025-06-11 16:08:10 +0530

    Code Optimization.

--- app/src/main/java/com/example/fcm/ChatState.kt
+++ app/src/main/java/com/example/fcm/ChatState.kt
@@ -1,16 +1,13 @@
 package com.example.fcm
 
-data class ChatState(
-    val isLoading: Boolean = false,
-    val errorMessage: String? = null,
-    val registrationSuccess: Boolean = false,
-    val customerId: String = "",
-    val messageText: String = ""
-)
-
 data class FcmState(
     val isLoading: Boolean = false,
     val isSuccess: Boolean = false,
     val errorMessage: String? = null,
     val customerId: String = ""
+)
+
+data class RegisterTokenDto(
+    val customer_id: String,
+    val fcm_token: String
 )
